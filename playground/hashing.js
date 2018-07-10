4f8b815e922537dcf42ff557cdf6b7b4ee961f21
ssdorwat <ssdorwat@ncsu.edu>
2018-07-10 17:06:43 -0400

    added bcrypt for password hashing

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,8 +1,19 @@
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+const password = 'password'
+bcrypt.genSalt(10,(err, salt) => {
+    bcrypt.hash(password, salt, (err, hash) => {
+        console.log(hash);
+    })
+})
 
+bcrypt.compare(password, '$2a$10$xllbZx151vDVC0qW6cjTkuKiYt2wZGfmVxfcdlBls0fUS1Kj1pJFK',
+(err, suc) => {
+    console.log(suc);
+})
 
-const token = jwt.sign('sahil123','secret123');
-console.log(token);
+// const token = jwt.sign('sahil123','secret123');
+// console.log(token);
 
-const data = jwt.verify(token, 'secret123');
-console.log(data);
+// const data = jwt.verify(token, 'secret123');
+// console.log(data);
